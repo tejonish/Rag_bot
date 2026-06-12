651ceb2122a962740b149aca898f30467390507a
Nishanth <nishanthb284@gmail.com>
2026-06-12 13:10:17 +0530

    WIP before secret cleanup

--- langgraph_version/llm.py
+++ langgraph_version/llm.py
@@ -3,11 +3,12 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
 
 def get_llm():
     return ChatGroq(
         model="llama-3.1-8b-instant",
         temperature=0,
+        GROQ_API_KEY = os.getenv("GROQ_API_KEY")
         
     )
